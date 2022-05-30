df221d80671f72e4817758960bc8b7ba7e278889
caotuankietc3a <caotuankietc3a@gmail.com>
2022-05-30 19:22:30 +0700

    Add Cpp compiler with input file

--- nvim/lua/caparies/compiler/cpp.lua
+++ nvim/lua/caparies/compiler/cpp.lua
@@ -7,36 +7,42 @@ local Terminal = require("toggleterm.terminal").Terminal
 
 local command_cpp = vim.api.nvim_exec(
 	[[
-  " let s:fname = input("File: ", "", "file")
-  let s:file = printf("%s %s %s %s\n", "g++ -std=c++11", expand('%'),  "&& ./a.out", "&& rm -f ./a.out")
-  function! s:My_Command_Cpp(file_path)
-    echo a:file_path
+  let s:cmd = printf("%s %s %s %s\n", "g++ -std=c++11", expand('%'),  "&& ./a.out", "&& rm -f ./a.out")
+  function! s:My_Command_Cpp(cmd)
+    echo a:cmd
   endfunction
 
-  call s:My_Command_Cpp(s:file)
+  call s:My_Command_Cpp(s:cmd)
 ]],
 	true
 )
 
-local cpp = Terminal:new({
-	cmd = command_cpp,
-	direction = "float",
-})
-
 function _CPP_TOGGLE()
+	local cpp = Terminal:new({
+		cmd = command_cpp,
+		direction = "float",
+	})
 	cpp:toggle()
 end
 
--- vim.api.nvim_create_user_command("CompileCppWithFile", function(opts)
--- 	print(string.upper(opts.args))
--- end, { nargs = 1 })
--- vim.cmd("CompileCppWithFile hello world") -- prints "HELLO WORLD"
---
--- local cpp_with_file = Terminal:new({
--- 	cmd = vim.cmd("CompileCppWithFile hello world"),
--- 	direction = "float",
--- })
---
--- function _CPP_FILE_TOGGLE()
--- 	cpp_with_file:toggle()
--- end
+local get_user_input = function(arg_val)
+	local cmd = vim.fn.printf(
+		"%s %s %s %s %s\n",
+		"g++ -std=c++11",
+		vim.fn.expand("%"),
+		"&& ./a.out",
+		arg_val,
+		"&& rm -rf a.out"
+	)
+	return cmd
+end
+
+function _CPP_FILE_TOGGLE()
+	local arg_val = vim.fn.input("Enter value of argv[1] = ")
+	vim.fn.printf("\n %s", get_user_input(arg_val))
+	local cpp_with_file = Terminal:new({
+		cmd = get_user_input(arg_val),
+		direction = "float",
+	})
+	cpp_with_file:toggle()
+end
