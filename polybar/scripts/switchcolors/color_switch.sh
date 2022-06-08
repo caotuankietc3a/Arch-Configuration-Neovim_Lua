8b8a6839d1223fb9387d4587ef0699d4295719d6
caotuankietc3a <caotuankietc3a@gmail.com>
2022-06-08 18:00:27 +0700

    Chaging polybar

--- polybar/scripts/switchcolors/color_switch.sh
+++ polybar/scripts/switchcolors/color_switch.sh
@@ -2,7 +2,7 @@
 
 SDIR="$HOME/.config/polybar/scripts/switchcolors"
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'Select Colors' -theme ~/.config/polybar/themes/tokyo_night_switch_theme.rasi <<< "嗀 tokyonight| tokyodark| dracula")"
+MENU="$(rofi -sep "|" -dmenu -i -p 'Select Colors' -theme ~/.config/polybar/themes/tokyo_night_switch_theme.rasi <<< "嗀 tokyonight| tokyodark| dracula")"
 
 case "$MENU" in
   *tokyonight) $SDIR/tokyonight/tokyonight.sh tokyonight ;;
