b08581173ae38f9a954e8684802237be9242cbe0
caotuankietc3a <caotuankietc3a@gmail.com>
2022-07-08 06:23:10 +0700

    Moving to bspwm window manager

--- nvim/lua/caparies/colorscheme.lua
+++ nvim/lua/caparies/colorscheme.lua
@@ -5,7 +5,7 @@ vim.cmd([[
     " darkplus
     " dracula
     " catppuccin
-  colorscheme tokyonight
+  colorscheme dracula
   catch /^Vim\%((\a\+)\)\=:E185/
     colorscheme default
     set background=dark
