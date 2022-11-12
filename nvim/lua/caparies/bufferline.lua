c736082ba60ba8ddd123be1380b520a7d6414c7d
caotuankietc3a <caotuankietc3a@gmail.com>
2022-11-13 00:24:08 +0700

    [feat]: Add multiple name in bufferline

--- nvim/lua/caparies/bufferline.lua
+++ nvim/lua/caparies/bufferline.lua
@@ -63,11 +63,13 @@ bufferline.setup({
     show_buffer_close_icons = true,
     show_close_icon = true,
     show_tab_indicators = true,
+    show_duplicate_prefix = true,
+    enforce_regular_tabs = false,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
     separator_style = "thin", -- | "thick" | "padded_slant | "thin" | { 'any', 'any' },
-    enforce_regular_tabs = true,
+    --[[ enforce_regular_tabs = true, ]]
     always_show_bufferline = true,
     -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
     --   -- add custom logic
